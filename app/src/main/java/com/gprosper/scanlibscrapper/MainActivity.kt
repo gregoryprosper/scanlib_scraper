48666a114a33a1494a3271fb54224a571dc60b70
Gregory Prosper <gregoryprosper@gmail.com>
2025-03-25 22:13:59 -0400

    Misc

--- app/src/main/java/com/gprosper/scanlibscrapper/MainActivity.kt
+++ app/src/main/java/com/gprosper/scanlibscrapper/MainActivity.kt
@@ -6,13 +6,16 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.countTxtView
 import kotlinx.android.synthetic.main.activity_main.goButton
 import kotlinx.android.synthetic.main.activity_main.resultsEditText
+import kotlinx.android.synthetic.main.activity_main.scrapeButton
 import kotlinx.android.synthetic.main.activity_main.urlEditText
 import kotlinx.android.synthetic.main.activity_main.webView
 import java.util.HashSet
@@ -21,13 +24,14 @@ class MainActivity : AppCompatActivity() {
 
     private val set = HashSet<String>()
 
-    @SuppressLint("SetJavaScriptEnabled", "AddJavascriptInterface")
+    @SuppressLint("SetJavaScriptEnabled", "AddJavascriptInterface", "JavascriptInterface")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupListeners()
 
         webView.settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(this, "HTMLOUT")
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
                 if (request?.url?.toString()?.contains("goto") == true) {
@@ -64,11 +68,15 @@ class MainActivity : AppCompatActivity() {
         goButton.setOnClickListener {
             webView.loadUrl(urlEditText.text.toString())
         }
+        scrapeButton.setOnClickListener {
+            startScraping()
+        }
 
         resultsEditText.setOnLongClickListener {
             val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
             val clip = ClipData.newPlainText("ScanlibText", resultsEditText.text.toString())
             clipboard!!.setPrimaryClip(clip)
+            set.clear()
             resultsEditText.text.clear()
             Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_LONG).show()
             return@setOnLongClickListener true
@@ -81,15 +89,9 @@ class MainActivity : AppCompatActivity() {
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
                 if (request?.url?.host?.contains("rapidgator.net") == true) {
-                    if (!set.contains(request.url.toString())) {
-                        set.add(request.url.toString())
-                        resultsEditText.text.append("${request.url}\n\n")
-                    }
+                    addUrl(request.url.toString())
                 } else if (request?.url?.host?.contains("turbobit.net") == true) {
-                    if (!set.contains(request.url.toString())) {
-                        set.add(request.url.toString())
-                        resultsEditText.text.append("${request.url}\n\n")
-                    }
+                    addUrl(request.url.toString())
                 }
                 return false
             }
@@ -100,5 +102,36 @@ class MainActivity : AppCompatActivity() {
                 "Referer" to from
             )
         )
+    }
+
+    @Synchronized
+    fun addUrl(url: String) {
+        if (set.contains(url)) return
+        set.add(url)
+        countTxtView.text = "Found: ${set.size} links"
+        resultsEditText.setText(set.joinToString(separator = "\n\n"))
+    }
+
+    private fun startScraping() {
+        Thread {
+            var index = 0
+            while(index < 50) {
+                runOnUiThread {
+                    webView.loadUrl(getScrapingJavaScript(index))
+                }
+                index++
+                Thread.sleep(1000)
+            }
+        }.start()
+    }
+
+    private fun getScrapingJavaScript(index: Int): String {
+        return """
+            javascript:(function() {
+                const elements = document.getElementsByClassName('rewlink');
+                const elementsArray = [].slice.call(elements);
+                elementsArray[$index].click();
+            })()
+        """.trimIndent()
     }
 }
